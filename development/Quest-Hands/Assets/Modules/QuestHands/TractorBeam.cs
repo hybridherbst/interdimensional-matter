22d8a8674abc6fbc2ebc1217990469beb92b5eb7
Felix Herbst <herbst@prefrontalcortex.de>
2020-02-01 23:30:23 +0100

    tractor v1

--- development/Quest-Hands/Assets/Modules/QuestHands/TractorBeam.cs
+++ development/Quest-Hands/Assets/Modules/QuestHands/TractorBeam.cs
@@ -6,13 +6,26 @@ public class TractorBeam : MonoBehaviour
 {
     public HandDebugInfo info;
     public float force = 1f;
+    public float particleEmission = 20f;
     public LayerMask layerMask;
+
+    public Transform visuals;
+
+    Vector3 forceCenter => info.averageRootPos + info.handDirection * 0.2f;
+
+    public ParticleSystem system;
+
+    void Update() {
+        var em = system.emission;
+        em.rateOverTimeMultiplier = info.force * particleEmission;
+
+        visuals.transform.position = forceCenter;
+        visuals.rotation = Quaternion.LookRotation(info.handDirection);
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        var forceCenter = info.averageRootPos + info.handDirection * 0.2f;
-
         if(Physics.SphereCast(info.averageRootPos - info.handDirection * 0.2f, 0.2f, info.handDirection, out var hitInfo, Mathf.Infinity, layerMask)) {
             Debug.Log("hit! : " + hitInfo.collider.name, hitInfo.collider);
 
