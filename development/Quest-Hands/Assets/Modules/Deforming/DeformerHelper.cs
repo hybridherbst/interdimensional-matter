0bacf3f5880ed63ccdd51f6e6981a82376db8c6e
Felix Herbst <herbst@prefrontalcortex.de>
2020-02-01 15:46:33 +0100

    deformer tests

---
+++ development/Quest-Hands/Assets/Modules/Deforming/DeformerHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeformerHelper : MonoBehaviour
+{
+    public Transform deformerParent;
+
+    [ContextMenu("Deform Now")]
+    void Start()
+    {
+        var defs = deformerParent.GetComponentsInChildren<Deform.Deformer>();
+        var def = GetComponent<Deform.Deformable>();
+        foreach(var d in defs)
+            def.DeformerElements.Add(new Deform.DeformerElement(d, true));
+
+        def.enabled = true;
+    }
+
+    [ContextMenu("Reset Now")]
+    void Reset() {
+        var def = GetComponent<Deform.Deformable>();
+        def.DeformerElements.Clear();
+        def.enabled = false;
+    }
+}
