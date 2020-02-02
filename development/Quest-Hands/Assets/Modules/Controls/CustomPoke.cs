7305b4598773a79f28e1581deefa06f59d8463e5
Felix Herbst <herbst@prefrontalcortex.de>
2020-02-02 14:20:56 +0100

    added button

---
+++ development/Quest-Hands/Assets/Modules/Controls/CustomPoke.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using OculusSampleFramework;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CustomPoke : MonoBehaviour
+{
+    public float localY;
+    public float activationMinY = -0.1f;
+    public float resetMinY = -0.04f;
+
+    private void Start() {
+        
+    }
+
+    bool pressed = false;
+
+    public UnityEvent OnPressed; 
+
+    private void Update() {
+        localY = transform.localPosition.y;
+
+        if(localY < activationMinY && !pressed) {
+            Debug.Log("OHMYGOD BUTTON PRESSED");
+            OnPressed.Invoke();
+            pressed = true;
+        }
+
+        if(localY > resetMinY && pressed)
+        {
+            pressed = false;
+        }
+    }
+}
