30308d8f95238e2f4189b7fc229b2d45946e83e5
Felix Herbst <herbst@prefrontalcortex.de>
2020-02-03 14:35:33 +0100

    added reserialization script

---
+++ development/Quest-Hands/Assets/Modules/Editor/ForceUpgradeAssetDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public static class ForceUpgradeAssetDatabase
+{
+    [MenuItem("pfc/Force AssetDatabase Upgrade")]
+    // Start is called before the first frame update
+    static void Upgrade()
+    {
+        AssetDatabase.ForceReserializeAssets();
+    }
+}
