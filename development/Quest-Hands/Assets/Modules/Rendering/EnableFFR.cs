38334bfbd75d90a86e002a6ff3d05d075602a4b9
Felix Herbst <herbst@prefrontalcortex.de>
2020-02-05 14:52:21 +0100

    enabled FFR (via "old" OVRPlugin way)

---
+++ development/Quest-Hands/Assets/Modules/Rendering/EnableFFR.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.XR.Oculus;
+
+public class EnableFFR : MonoBehaviour
+{
+    public OVRPlugin.FixedFoveatedRenderingLevel ffrLevel = OVRPlugin.FixedFoveatedRenderingLevel.HighTop;
+    void Start()
+    {
+        // Unity.XR.Oculus.Utils.SetFoveationLevel(null);
+        OVRPlugin.fixedFoveatedRenderingLevel = ffrLevel;
+    }
+}
