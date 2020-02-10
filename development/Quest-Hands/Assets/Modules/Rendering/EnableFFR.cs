f37b889e825878f8abd703e8733380ee096bdbea
Felix Herbst <herbst@prefrontalcortex.de>
2020-02-10 18:39:26 +0100

    ffr script updated

--- development/Quest-Hands/Assets/Modules/Rendering/EnableFFR.cs
+++ development/Quest-Hands/Assets/Modules/Rendering/EnableFFR.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.XR.Oculus;
+using UnityEngine.Rendering.Universal;
 
 public class EnableFFR : MonoBehaviour
 {
@@ -10,10 +11,22 @@ public class EnableFFR : MonoBehaviour
     {
         // Unity.XR.Oculus.Utils.SetFoveationLevel(null);
         OVRPlugin.fixedFoveatedRenderingLevel = ffrLevel;
+
+        StartCoroutine(_LogLog());
+    }
+
+    IEnumerator _LogLog()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            LogCamState();
+        }
     }
 
     [ContextMenu("Log Cam State")]
     void LogCamState() {
-        Debug.Log(UnityEngine.XR.XRSettings.stereoRenderingMode);
+        // Debug.Log(UnityEngine.XR.XRSettings.stereoRenderingMode);
+        Debug.Log("can use single pass: " + UniversalRenderPipeline.CanXRSDKUseSinglePass(Camera.main));
     }
 }
